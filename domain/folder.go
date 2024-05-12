ecc26cc2af4704818217cbad0bcecf09c1d34154
Ed <edventure317@gmail.ocm>
2024-05-12 15:24:21 +0800

    feat(domain): Implement Folder struct with essential methods
    includes methods such as AddFolder for adding folder while validating folder name, RemoveFolder for remove a folder,
    Rename for rename a folder, and GetSortedFolders for sorted of folders and FoldersFormatted for formatted the folder info of the folders slice.

--- domain/folder.go
+++ domain/folder.go
@@ -17,7 +17,15 @@ type (
 	Folder struct {
 		Name        string
 		Created     time.Time
+		TheFiles    TheFiles
 		Description string
+	}
+
+	// TheFiles interface defines the methods to manage files within a folder.
+	TheFiles interface {
+		AddFile(name, desc string) error
+		RemoveFile(name string) error
+		ListFiles() ([]string, error)
 	}
 )
 
@@ -27,4 +35,38 @@ func NewFolders() *Folders {
 	return &Folders{
 		List: make(map[string]*Folder),
 	}
+}
+
+// AddFolder adds a new folder to the collection with the given name.
+func (f *Folders) AddFolder(name, description string) error {
+
+	return nil
+}
+
+// RemoveFolder removes a folder from the collection by its name.
+func (f *Folders) RemoveFolder(name string) error {
+
+	return nil
+}
+
+// Rename rename a folder,
+func (f *Folders) Rename(name, newName string) error {
+
+	return nil
+}
+
+// GetSortedFolders returns a sorted slice of Folder based on the provided sort type and order.
+func (f *Folders) GetSortedFolders(by sortBy, order sortOrder) []Folder {
+
+	return nil
+}
+
+// FoldersFormatted formats a slice of Folder structures into a string slice
+// for display purposes. The format includes the folder name, optional description,
+// creation date, and associated user.
+// Example output:
+// "folder1 2023-01-01 15:00:00 user1", "folder2 this-is-folder-2 2023-01-01 15:00:10 user1"
+func FoldersFormatted(user string, folders []Folder) []string {
+
+	return nil
 }
--- domain/utils.go
+++ domain/utils.go
@@ -2,6 +2,22 @@ package domain
 
 import "unicode"
 
+// sortBy it's an enumerate used to specify the sorting type.
+type sortBy int
+
+const (
+	sortByName sortBy = iota
+	sortByCreated
+)
+
+// sortOrder it's an enumerate used to specify the sort order.
+type sortOrder int
+
+const (
+	ascending sortOrder = iota
+	descending
+)
+
 // containsSpace checks if the input string contains any space characters.
 func containsSpace(s string) bool {
 	for _, r := range s {
